50186e752252860c125412635c41d17704af1d66
A-Siam <abdullah.khaled.kamal0@gmail.com>
2019-12-07 08:58:44 +0200

    add load store control => controller

--- src/controller.sv
+++ src/controller.sv
@@ -11,28 +11,30 @@ module controller(input   logic        clk, reset,
                   output  logic       regdstE, regwriteE, 
                   output  logic       regwriteM, regwriteW,
                   output  logic       jumpD, extendD,
-                  output  logic [2:0] alucontrolE);
+                  output  logic [2:0] alucontrolE,
+                  output logic [1:0] ls_ctrlM);
 				  
- logic [1:0] aluopD;
- logic       memtoregD, memwriteD, alusrcD,
+ logic       memtoregD, ls_ctrlD ,memwriteD, alusrcD,
             regdstD, regwriteD;
  logic [2:0] alucontrolD;
  logic       memwriteE;
- maindec md(opD, memtoregD, memwriteD, branchD, bneD,
+ logic [1:0] aluopD, ls_ctrlE;
+
+ maindec md(opD, memtoregD, ls_ctrlD ,memwriteD, branchD, bneD,
             alusrcD, regdstD, regwriteD, jumpD, extendD,
             aluopD);
  aludec  ad(functD, aluopD, alucontrolD);
  assign pcsrcD = (branchD & equalD) | (bneD & nequalD);
  
  // pipeline registers
- floprc #(8) regE(clk, reset, flushE,
-                 {memtoregD, memwriteD, alusrcD, 
+ floprc #(10) regE(clk, reset, flushE,
+                 {memtoregD, ls_ctrlD, memwriteD, alusrcD, 
                   regdstD, regwriteD, alucontrolD}, 
-                 {memtoregE, memwriteE, alusrcE, 
+                 {memtoregE, ls_ctrlE, memwriteE, alusrcE, 
                   regdstE, regwriteE,  alucontrolE});
  flopr #(3) regM(clk, reset, 
-                 {memtoregE, memwriteE, regwriteE},
-                 {memtoregM, memwriteM, regwriteM});
+                 {memtoregE, ls_ctrlE,memwriteE, regwriteE},
+                 {memtoregM, ls_ctrlM,memwriteM, regwriteM});
  flopr #(2) regW(clk, reset, 
                  {memtoregM, regwriteM},
                  {memtoregW, regwriteW});
